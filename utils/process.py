9d1373f1dfc50cb9acb6016e8d609d5e07e03a92
l3uddz <l3uddz@gmail.com>
2017-11-12 15:22:34 +0000

    strip ' from returned execute output

--- utils/process.py
+++ utils/process.py
@@ -9,7 +9,7 @@ def execute(command, callback=None):
     total_output = ''
     process = subprocess.Popen(shlex.split(command), shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     while True:
-        output = str(process.stdout.readline()).lstrip('b').replace('\\n', '')
+        output = str(process.stdout.readline()).lstrip('b').replace('\\n', '').strip("'")
         if process.poll() is not None:
             break
         if output and len(output) > 6:
