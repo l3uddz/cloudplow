dcce010312ddaf13a345f413148a5e46d4b45e8c
l3uddz <l3uddz@gmail.com>
2017-11-19 16:21:25 +0000

    minor change to scaleway class

--- utils/syncer/scaleway.py
+++ utils/syncer/scaleway.py
@@ -5,16 +5,16 @@ log = logging.getLogger("scaleway")
 
 class Scaleway:
     def __init__(self, from_config, to_config, **kwargs):
-        self.kwargs = kwargs
         self.sync_from_config = from_config
         self.sync_to_config = to_config
+        self.kwargs = kwargs
 
         # pass region from kwargs (default France)
         if 'region' in kwargs:
             self.region = kwargs['region']
         else:
             self.region = 'par1'
-        # pass commercial-type from kwargs (default X64-2GB)
+        # pass type from kwargs (default X64-2GB)
         if 'type' in kwargs:
             self.type = kwargs['type']
         else:
