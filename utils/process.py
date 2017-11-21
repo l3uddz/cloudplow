2e1703e5385cec69029c080ef8697027eccaf5b6
l3uddz <l3uddz@gmail.com>
2017-11-21 04:28:59 +0000

    change process execute stripping.

--- utils/process.py
+++ utils/process.py
@@ -10,7 +10,7 @@ def execute(command, callback=None, logs=True, shell=False):
     process = subprocess.Popen(shlex.split(command) if not shell else command, shell=shell, stdout=subprocess.PIPE,
                                stderr=subprocess.STDOUT)
     while True:
-        output = str(process.stdout.readline()).lstrip('b').replace('\\n', '')
+        output = process.stdout.readline().decode().replace("\r\n", '').strip()
         if process.poll() is not None:
             break
         if output and len(output) and output != "''":
