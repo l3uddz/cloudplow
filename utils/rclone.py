4b162d6916834565443651e22eae8f2c39ebe61b
James <l3uddz@gmail.com>
2018-06-21 14:06:07 +0100

    Update rclone.py
    dont escape excludes

--- utils/rclone.py
+++ utils/rclone.py
@@ -1,5 +1,4 @@
 import logging
-import re
 import time
 from urllib.parse import urljoin
 
@@ -97,7 +96,7 @@ class RcloneUploader:
 
     def __excludes2string(self):
         return ' '.join(
-            "--exclude=%s" % (cmd_quote(re.escape(value)) if isinstance(value, str) else value) for value in
+            "--exclude=%s" % (cmd_quote(value) if isinstance(value, str) else value) for value in
             self.config['rclone_excludes']).replace('=None', '').strip()
 
 
